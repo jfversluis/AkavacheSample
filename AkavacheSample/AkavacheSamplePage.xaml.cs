f0e18aa4f011ca210e2018bfa5266fcdf59476e4
Gerald Versluis <gerald@verslu.is>
2017-08-17 12:43:03 +0200

    Initial commit

---
+++ AkavacheSample/AkavacheSamplePage.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Akavache;
+using BeenPwned.Api;
+using BeenPwned.Api.Models;
+using MvvmHelpers;
+using Xamarin.Forms;
+
+namespace AkavacheSample
+{
+    public partial class AkavacheSamplePage : ContentPage
+    {
+        public bool IsLoading;
+
+		private readonly ObservableRangeCollection<Breach> _breaches = new ObservableRangeCollection<Breach>();
+		public ObservableCollection<Breach> Breaches => _breaches;
+
+		private ICommand refreshCommand;
+        public ICommand RefreshCommand
+        {
+            get
+            {
+                return refreshCommand ??
+                    (refreshCommand = new Command(() => LoadBreaches(), () => !IsLoading));
+            }
+        }
+
+		private IBeenPwnedClient _pwnedClient = new BeenPwnedClient($"AkavacheSample");
+
+		public AkavacheSamplePage()
+        {
+            InitializeComponent();
+
+            BindingContext = this;
+
+            RefreshCommand.Execute(null);
+        }
+
+		private void LoadBreaches()
+		{
+			IsLoading = true;
+
+			GetBreaches(true);
+			
+            IsLoading = false;
+		}
+
+		public void GetBreaches(bool force = false)
+		{
+			var cache = BlobCache.LocalMachine;
+			cache.GetAndFetchLatest("breaches", GetRemoteBreachesAsync,
+				offset =>
+				{
+					TimeSpan elapsed = DateTimeOffset.Now - offset;
+					var invalidateCache = (force || elapsed > new TimeSpan(24, 0, 0));
+					return invalidateCache;
+				})
+				.Subscribe((cities) =>
+				{
+                    _breaches.ReplaceRange(cities);
+				});
+		}
+
+		private async Task<IEnumerable<Breach>> GetRemoteBreachesAsync()
+		{
+            return await _pwnedClient.GetAllBreaches();
+		}
+    }
+}
---
+++ AkavacheSample/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Akavache;
+using Xamarin.Forms;
+
+namespace AkavacheSample
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            BlobCache.ApplicationName = "AkavacheSample";
+
+            MainPage = new NavigationPage(new AkavacheSamplePage());
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
